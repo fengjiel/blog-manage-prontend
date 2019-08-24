fdf83d02e97ac5d46cd44ae21cbcbda3f8615d58
kianakislana777 <youknew10086@gmail.com>
2019-08-25 00:25:08 +0800

    1

---
+++ src/components/ArticleEditor.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="body">
+        <mark-down/>
+
+    </div>
+</template>
+
+<script>
+    import MarkDown from 'vue-meditor'
+
+    export default {
+        name: "ArticleEditor",
+        components: {
+            MarkDown
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        }
+
+    }
+</script>
+
+<style scoped>
+    #body {
+    }
+
+</style>
---
+++ src/components/Upload.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <el-upload
+                class="upload-demo"
+                action="http://localhost/api/v1/manage/article/upload.do"
+                :on-preview="handlePreview"
+                :on-remove="handleRemove"
+                :file-list="fileList"
+                list-type="picture/text">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+    export default {
+        name: "Upload",
+        data() {
+            return {
+                fileList: [{
+                    name: 'file',
+                    url: ''
+                }]
+            };
+        },
+        methods: {
+            handleRemove(file, fileList) {
+                Console.log(file, fileList);
+            },
+            handlePreview(file) {
+                Console.log(file);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
